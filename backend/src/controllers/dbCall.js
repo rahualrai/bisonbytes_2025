87a84cff17354b1841b2bfef259a484271061493
rahualrai <rairahual7@gmail.com>
2025-03-23 03:20:26 -0400

    Add getVitalsByResponseId endpoint to retrieve vitals by responseId

--- backend/src/controllers/dbCall.js
+++ backend/src/controllers/dbCall.js
@@ -10,13 +10,24 @@ export const getVitals = async (req, res) => {
     }
 };
 
-
 export const getLatestVitals = async (req, res) => {
 
     try {
         const latestVitals = await VitalsTesting.findOne().sort({ timestamp: -1 });
         res.status(200).json(latestVitals);
     } catch (error) {
+        res.status(500).json({ status: "error", message: error.message });
+    }
+};
+
+// to query db using responseId
+export const getVitalsByResponseId = async (req, res) => {
+    console.log("Request body:", req.body);
+    try {
+        const vitals = await VitalsTesting.findOne  ({ responseId: req.body.responseId });
+        res.status(200).json(vitals);
+    }
+    catch (error) {
         res.status(500).json({ status: "error", message: error.message });
     }
 };
--- backend/src/routes/vitalsRoutes.js
+++ backend/src/routes/vitalsRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { receiveVitals, getPatientVitals } from "../controllers/VitalsController.js";
-import { getVitals, getLatestVitals } from "../controllers/dbCall.js"; // Import the getVitals function
+import { getVitals, getLatestVitals, getVitalsByResponseId } from "../controllers/dbCall.js"; // Import the getVitals function
 
 const router = express.Router();
 
 router.post("/", receiveVitals);
 router.get("/get-vitals", getVitals); 
 router.get("/get-latest-vitals", getLatestVitals);
+router.post("/get-vitals-by-responseId", getVitalsByResponseId);
 
 export default router;
